58717eda4ad527bc0ca35c936decb7adbb37233c
Vavassor <copernicus-@hotmail.com>
2018-02-08 21:18:43 -0500

    Adds missing colours .h and .cpp files.

---
+++ colours.cpp
@@ -0,0 +1,19 @@
+#include "colours.h"
+
+extern const Vector3 vector3_red     = {1.0f, 0.0f, 0.0f};
+extern const Vector3 vector3_green   = {0.0f, 1.0f, 0.0f};
+extern const Vector3 vector3_blue    = {0.0f, 0.0f, 1.0f};
+extern const Vector3 vector3_cyan    = {0.0f, 1.0f, 1.0f};
+extern const Vector3 vector3_magenta = {1.0f, 0.0f, 1.0f};
+extern const Vector3 vector3_yellow  = {1.0f, 1.0f, 0.0f};
+extern const Vector3 vector3_black   = {0.0f, 0.0f, 0.0f};
+extern const Vector3 vector3_white   = {1.0f, 1.0f, 1.0f};
+
+extern const Vector4 vector4_red     = {1.0f, 0.0f, 0.0f, 1.0f};
+extern const Vector4 vector4_green   = {0.0f, 1.0f, 0.0f, 1.0f};
+extern const Vector4 vector4_blue    = {0.0f, 0.0f, 1.0f, 1.0f};
+extern const Vector4 vector4_cyan    = {0.0f, 1.0f, 1.0f, 1.0f};
+extern const Vector4 vector4_magenta = {1.0f, 0.0f, 1.0f, 1.0f};
+extern const Vector4 vector4_yellow  = {1.0f, 1.0f, 0.0f, 1.0f};
+extern const Vector4 vector4_black   = {0.0f, 0.0f, 0.0f, 1.0f};
+extern const Vector4 vector4_white   = {1.0f, 1.0f, 1.0f, 1.0f};
---
+++ colours.h
@@ -0,0 +1,24 @@
+#ifndef COLOURS_H_
+#define COLOURS_H_
+
+#include "vector_math.h"
+
+extern const Vector3 vector3_red;
+extern const Vector3 vector3_green;
+extern const Vector3 vector3_blue;
+extern const Vector3 vector3_cyan;
+extern const Vector3 vector3_magenta;
+extern const Vector3 vector3_yellow;
+extern const Vector3 vector3_black;
+extern const Vector3 vector3_white;
+
+extern const Vector4 vector4_red;
+extern const Vector4 vector4_green;
+extern const Vector4 vector4_blue;
+extern const Vector4 vector4_cyan;
+extern const Vector4 vector4_magenta;
+extern const Vector4 vector4_yellow;
+extern const Vector4 vector4_black;
+extern const Vector4 vector4_white;
+
+#endif // COLOURS_H_
